3e550866ccb496ac1c2c7dd71a3d2aa2076cd850
David Espino <davidespinog@gmail.com>
2022-06-21 13:30:47 -0500

    new funciton for upÃloading your own cats image

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=6cd2e2ec-ea37-44bb-9f4a-5be4c8582143'
-const URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?&api_key=6cd2e2ec-ea37-44bb-9f4a-5be4c8582143'
-const URL_DELETE_FAVOURITES = (id) => `https://api.thecatapi.com/v1/favourites/${id}?&api_key=6cd2e2ec-ea37-44bb-9f4a-5be4c8582143`;
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=2'
+const URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites'
+const URL_DELETE_FAVOURITES = (id) => `https://api.thecatapi.com/v1/favourites/${id}?`;
+const URL_UPLOAD = 'https://api.thecatapi.com/v1/images/upload'
 
 const spanError = document.getElementById('error')
 
@@ -23,7 +24,12 @@ async function loadRandomCats() {
     }
 }
 async function loadFavouriteCats() {
-    const result = await fetch(URL_FAVOURITES);
+    const result = await fetch(URL_FAVOURITES, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': '6cd2e2ec-ea37-44bb-9f4a-5be4c8582143', 
+        },
+    });
     const data = await result.json();
     if(result.status !== 200) {
         spanError.innerText = "Hubo un error: " + result.status + data.message
@@ -55,7 +61,8 @@ async function saveFavouriteCat(id) {
     const result = await fetch(URL_FAVOURITES, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-API-KEY': '6cd2e2ec-ea37-44bb-9f4a-5be4c8582143'
         },
         body: JSON.stringify({
             image_id: id
@@ -72,6 +79,9 @@ async function saveFavouriteCat(id) {
 async function deleteFavouriteCat(id){
     const result = await fetch(URL_DELETE_FAVOURITES(id), {
         method: 'DELETE',
+        headers: {
+            'X-API-KEY': '6cd2e2ec-ea37-44bb-9f4a-5be4c8582143'
+        }
     });
     const data = await result.json()
     if(result.status !== 200) {
@@ -83,5 +93,32 @@ async function deleteFavouriteCat(id){
 const catButton = document.getElementById('catButton');
 catButton.onclick = loadRandomCats;
 
+async function uploadCatImg() {
+    const form = document.getElementById('uploadingForm');
+    const formData = new FormData(form);
+
+    const result = await fetch(URL_UPLOAD, {
+        method: 'POST', 
+        headers: {
+            // Se quita el Content-Type en este caso debido a que la instancia
+            // formData ya se encarga de ese paso
+            // 'Content-Type': 'multipart/form-data', 
+            'X-API-KEY': '6cd2e2ec-ea37-44bb-9f4a-5be4c8582143',
+        },
+        body: formData,
+    })
+    const data = await result.json()
+    if (result.status !== 201) {
+        spanError.innerHTML = `Hubo un error al subir michi: ${result.status} ${data.message}`
+
+    } else {
+        console.log("Foto de michi cargada :)");
+        console.log(result)}
+        console.log(data);
+        console.log(data.url);
+    saveFavouriteCat(data.id)
+}
 loadRandomCats()
 loadFavouriteCats()
+
+//id foto balam CsWFbQUXQ
